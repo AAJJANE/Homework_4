cf1c5d64e35dcdf7612ed3311b2fca81c6cc693a
Jane Ar <83021522+AAJJANE@users.noreply.github.com>
2024-02-27 19:33:11 +0300

    Update MainActivity.java

--- Homework_4/app/src/main/java/com/aae/homework_4/MainActivity.java
+++ Homework_4/app/src/main/java/com/aae/homework_4/MainActivity.java
@@ -12,7 +12,6 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    // поля
     private TextView screen, coordinates;
     private float x;
     private float y;
